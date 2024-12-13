a27667ae62c4cdf6f43eabe07a1f8231221dc8a1
Victoria Oemer <victoria.oemer@gmail.com>
2024-12-13 12:29:48 +0100

    add basic routes

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dashboard</h1>
+  <p>Dashboard content goes here</p>
+  <RouterLink to="/">Go to Welcome Page</RouterLink>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'DashboardPage'
+}
+</script>
---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1> WelcomePage</h1>
+  <p> and unser name input </p>
+  <RouterLink to="/dashboard"> Go to Dashboard</RouterLink>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'WelcomePage'
+}
+</script>

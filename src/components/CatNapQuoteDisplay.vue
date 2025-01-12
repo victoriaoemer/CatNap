dd491c9ee7e0c60762d565e50c3b3fb21a35044f
HeleneReiter <reiterhelene648@gmail.com>
2025-01-12 11:06:42 +0100

    styling responsive image

--- src/components/CatNapQuoteDisplay.vue
+++ src/components/CatNapQuoteDisplay.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex flex-col justify-between">
     <div class="pb-12 md:pb-3 h-full">
-      <p class="mb-3 font-bold lg:text-3xl md:text-2xl text-gradient-blue w-fit">Today's Picture</p>
+      <p class="mb-3 font-bold text-3xl text-gradient-blue w-fit">Today's Picture</p>
       </div>
     </div>
-    <div v-if="image" class="relative top-14">
+    <div v-if="image" class="relative lg:top-10">
   <img
     src="@/assets/cat-images/cat-home2.svg"
     alt="Cat"
-    class="h-20 absolute -top-20 right-0"
+    class="h-16 absolute -top-16 right-0"
   />
   <div class="flex justify-center">
     <img
       :src="image"
       alt="Pixabay Image"
-      class="rounded-xl border-2 border-emotion-green w-full"
+      class="rounded-xl border-2 object-fit border-emotion-green w-full"
     />
   </div>
 </div>

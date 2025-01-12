95498a2248bb2b489f2ed040f1a4bb0255f4d85f
HeleneReiter <reiterhelene648@gmail.com>
2025-01-12 11:01:57 +0100

    styling Image

--- src/components/CatNapQuoteDisplay.vue
+++ src/components/CatNapQuoteDisplay.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="flex flex-col justify-between">
-    <div v-if="image" class="relative">
-      <img
-        src="@/assets/cat-images/cat-home2.svg"
-        alt="Cat"
-        class="h-16 absolute -top-16 right-0 pt-5"
-      />
-      <div class="flex justify-center">
-        <img
-          :src="image"
-          alt="Pixabay Image"
-          class="rounded-xl border-2 border-emotion-green object-cover w-full h-36 "
-        />
+    <div class="pb-12 md:pb-3 h-full">
+      <p class="mb-3 font-bold lg:text-3xl md:text-2xl text-gradient-blue w-fit">Today's Picture</p>
       </div>
     </div>
-    <p v-else>No image available.</p>
+    <div v-if="image" class="relative top-14">
+  <img
+    src="@/assets/cat-images/cat-home2.svg"
+    alt="Cat"
+    class="h-20 absolute -top-20 right-0"
+  />
+  <div class="flex justify-center">
+    <img
+      :src="image"
+      alt="Pixabay Image"
+      class="rounded-xl border-2 border-emotion-green w-full"
+    />
   </div>
+</div>
+    <p v-else>No image available.</p>
 </template>
 
 <script setup lang="ts">

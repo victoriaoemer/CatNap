a9d66c44cfc33bf9afc5212eb5bb2736d9a15985
Laceels <s2410629006@students.fh-hagenberg.at>
2025-01-17 18:52:03 +0100

    added arrows to theme dropdown

--- src/components/CatNapSelect.vue
+++ src/components/CatNapSelect.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
-    <select
-      class="w-full bg-[#3B32AB] border border-[#3E2FE9] rounded-xl p-2 "
-      v-model="inputValue"
-    >
-    <option :value="null" disabled>{{ placeholder }}</option>
-    <option v-for="option in options" :key="option" :value="option">
-      {{ option }}
-    </option>
-  </select>
+  <div class="relative w-full">
+    <div class="flex items-center bg-[#3B32AB] border border-[#3E2FE9] rounded-xl p-2">
+      <select
+        class="flex-1 bg-transparent text-white appearance-none focus:outline-none"
+        v-model="inputValue"
+      >
+        <option :value="null" disabled>{{ placeholder }}</option>
+        <option v-for="option in options" :key="option" :value="option">
+          {{ option }}
+        </option>
+      </select>
+      <div class="flex flex-col justify-center items-center ml-2 pointer-events-none">
+        <!-- Pfeil nach oben -->
+        <svg class="w-3 h-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path
+            fill-rule="evenodd"
+            d="M14.78 11.28a.75.75 0 0 1-1.06 0L10 7.56l-3.72 3.72a.75.75 0 1 1-1.06-1.06l4.25-4.25a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <!-- Pfeil nach unten -->
+        <svg class="w-3 h-3 text-gray-400 -mt-1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path
+            fill-rule="evenodd"
+            d="M5.22 8.72a.75.75 0 0 1 1.06 0L10 12.44l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0l-4.25-4.25a.75.75 0 0 1 0-1.06Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,6 +67,7 @@ select {
 
 
   appearance: none;
+  padding-right: 2.5rem; /* Platz für das SVG */
 
 }
 
